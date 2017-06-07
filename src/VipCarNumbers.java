8fd88b00f76ec344bda029d6ceb3123b18a9433f
strorikaster <strorikaster@gmail.com>
2017-06-07 17:45:39 +0300

    VipCarNumbers07062017

--- src/VipCarNumbers.java
+++ src/VipCarNumbers.java
@@ -1,5 +1,7 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
+
+import static java.util.Collections.binarySearch;
 
 /**
  * Created by в on 20.05.2017.
@@ -11,22 +13,24 @@ public class VipCarNumbers {
         String[] carNumbers = new String[1000];
         String[] regionNumbers = new String[100];
         int[] additionalRegionNumbers = {102, 103, 113, 116, 121, 123, 124, 125, 126, 134, 136, 138, 142, 150, 190, 750, 152, 154, 159, 161, 163, 164, 196, 173, 174, 177, 197, 199, 777, 178, 186};
+        String[] rnWithAdditional = new String[regionNumbers.length + additionalRegionNumbers.length] ;
         ArrayList<String> regionNumbersList = new ArrayList<>();
 
         //ArrayList <Integer>  rn = regionNumbers + additionalRegionNumbers;
-
+        String[] arrVipNumbers = new String[2500000];
         // String[] rn = new String[99];
 
         ArrayList<String> vipNumbers = new ArrayList<>();
+        //Object[] arrVipNumbers = new Object[2000000];
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
 
         for (int i = 0; i <= carNumbers.length - 1; i++) {
             carNumbers[i] = Integer.toString(i);
-           if (Integer.parseInt(carNumbers[i]) < 10){
-               carNumbers[i] = "00"  + Integer.toString(i);
-               //System.out.println(carNumbers[i]);
-           }
+            if (Integer.parseInt(carNumbers[i]) < 10){
+                carNumbers[i] = "00"  + Integer.toString(i);
+                //System.out.println(carNumbers[i]);
+            }
             if (Integer.parseInt(carNumbers[i]) >= 10 && Integer.parseInt(carNumbers[i]) < 100){
                 carNumbers[i] = "0" + Integer.toString(i);
                 //System.out.println(carNumbers[i]);
@@ -39,19 +43,22 @@ public class VipCarNumbers {
 
         for (int i = 0; i < regionNumbers.length - 1; i++) {
             regionNumbers[i] =  Integer.toString(i);
+            rnWithAdditional[i] = Integer.toString(i);
             if(Integer.parseInt(regionNumbers[i]) < 10) {
+                regionNumbers[i] = "0" + Integer.toString(i);
                 regionNumbers[i] = "0" + Integer.toString(i);
             }
             regionNumbersList.add(regionNumbers[i]);
         }
         for (int i = 0; i < additionalRegionNumbers.length - 1; i++) {
+
             regionNumbersList.add(Integer.toString(additionalRegionNumbers[i]));
         }
 
 
-        for (int i = 0; i <= allowedSymbols.length - 1; i++) {
-            for (int j = 0; j <= carNumbers.length - 1; j++) {
-                for (int k = 0; k <= regionNumbersList.size() - 1; k++) {
+        for (int i = 0; i < allowedSymbols.length; i++) {
+            for (int j = 0; j < carNumbers.length; j++) {
+                for (int k = 0; k < regionNumbersList.size(); k++) {
 
 //                    if (carNumbers[j] < 10 && Integer.parseInt(regionNumbersList.get(k)) < 10) {
 //                        vipNumbers.add(Character.toString(allowedSymbols[i]) + "00" + carNumbers[j] + allowedSymbols[i] + allowedSymbols[i] + "0" + regionNumbersList.get(k));
@@ -69,12 +76,15 @@ public class VipCarNumbers {
 //                                        vipNumbers.add(Character.toString(allowedSymbols[i]) + carNumbers[j] + allowedSymbols[i] + allowedSymbols[i] + regionNumbersList.get(k));
 //                                    }else{
 //                                        if (carNumbers[j] > 100 && Integer.parseInt(regionNumbersList.get(k)) > 100) {
-                                            vipNumbers.add(Character.toString(allowedSymbols[i]) + carNumbers[j] + allowedSymbols[i] + allowedSymbols[i] + regionNumbersList.get(k));
+                    vipNumbers.add(Character.toString(allowedSymbols[i]) + carNumbers[j] + allowedSymbols[i] + allowedSymbols[i] + regionNumbersList.get(k));
+//                    if (i > 0 && i < allowedSymbols.length-1){
+//                        vipNumbers.add(Character.toString(allowedSymbols[i+1]) + carNumbers[j] + allowedSymbols[i] + allowedSymbols[i] + regionNumbersList.get(k));
+//                    }
 //                                    }
 //                                    }
 //                                }
-                           // }
-                        //}
+                    // }
+                    //}
                     //}
 //
                 }
@@ -85,10 +95,11 @@ public class VipCarNumbers {
 
 
 
-//        for (String item : regionNumbersList) {
+//        for (String item : vipNumbers) {
 //            System.out.println(item);
 //        }
-//      System.out.println(vipNumbers.size());
+        arrVipNumbers = vipNumbers.toArray(new String[vipNumbers.size()]);
+    // System.out.println(vipNumbers.size());
 //
 //        PrintStream out = null;
 //        try {
@@ -102,29 +113,98 @@ public class VipCarNumbers {
 //            System.out.println(item1);
 //        }
 //
-//        for (String item : vipNumbers) {
-//            System.out.println(item);
-//        }
+
 
         //System.out.println(regionNumbersList.size());
 
-        for(;;) {
-            System.out.println("Please, enter car number in format xXXXxxXX, where x - letter, X - digit");
-            String enteredNumber = reader.readLine().trim();
-            //for (int i = 0; i < vipNumbers.size(); i++) {
-            if ((vipNumbers.contains(enteredNumber))) {
-                System.out.println("It's VIP car number");
-            } else {
-                System.out.println("It's no VIP car number");
-            }
+        //Collections.sort(sortVipNumbers);
+        //sortVipNumbers.addAll(vipNumbers);
+        String[] sortArrVipNumbers = Arrays.copyOf(arrVipNumbers, arrVipNumbers.length);
+        Arrays.sort(sortArrVipNumbers);
 
-            //}
-            if (enteredNumber.equals("STOP")){
-                System.out.println("Received \"STOP\" command. exit");
-                break;
-            }
-        }
+       ArrayList<String> sortVipNumbers =  new ArrayList<String>();
+        sortVipNumbers.addAll(vipNumbers);
+       Collections.sort(sortVipNumbers);
+
+        HashSet<String> hashSetVipNumbers = new HashSet<>();
+        hashSetVipNumbers.addAll(vipNumbers);
+
+        TreeSet<String> treeSetVipNumbers = new TreeSet<>();
+        treeSetVipNumbers.addAll(vipNumbers);
+
+//       System.out.println(vipNumbers);
+//        System.out.println(sortVipNumbers);
+//        System.out.println(hashSetVipNumbers);
+//        System.out.println(treeSetVipNumbers);
+
+       checkVipNumbers(arrVipNumbers, sortArrVipNumbers, hashSetVipNumbers, treeSetVipNumbers);
+
+//        for (String item : treeSetVipNumbers) {
+//            System.out.println(item);
+//        }
+
     }
+            public static void checkVipNumbers(String[] arrVipNumbers, String[] sortArrVipNumbers, HashSet<String> hashSetVipNumbers, TreeSet<String> treeSetVipNumbers) throws IOException {
 
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                long start = System.currentTimeMillis();
+                //String[] arrVipNumbers = new String[2500000];
+                //arrVipNumbers = vipNumbers.toArray();
+
+                for(;;) {
+                    System.out.println("Please, enter car number in format xXXXxxXX, where x - letter, X - digit");
+                    String enteredNumber = reader.readLine().trim();
+                    boolean a = false;
+
+                    System.out.println("Search without sorting");
+                    for (int i = 0; i < arrVipNumbers.length; i++) {
+                        if (arrVipNumbers[i].equals(enteredNumber)) {
+                            //System.out.println("true" + " " + (System.currentTimeMillis() - start) + " ms");
+                            //System.out.println(vipNumbers.get(i));
+                            a = true;
+                        } else {
+                            //System.out.println("false" + " " + (System.currentTimeMillis() - start) + " ms");
+                           // System.out.println(vipNumbers.get(i));
+                            a = false;
+                        }
+                    }
+                    if (a == true){
+                    System.out.println("true " + ((System.currentTimeMillis() - start)) + " ms");
+                    }else{
+                        System.out.println("false " + ((System.currentTimeMillis() - start)) + " ms");
+                    }
+
+                    System.out.println("Search with sorting");
+
+                    //if ((binarySearch(sortArrVipNumbers, enteredNumber) >= 0)) {
+                    if ((Arrays.binarySearch(sortArrVipNumbers, enteredNumber))>=0){
+                        System.out.println("true" + " " + (System.currentTimeMillis() - start) + " ms");
+                    } else {
+                        System.out.println("false" + " " + (System.currentTimeMillis() - start) + " ms");
+                    }
+
+                    System.out.println("Search HashSet");
+                    //Collections.sort(vipNumbers);
+                    if (hashSetVipNumbers.contains(enteredNumber)) {
+                        System.out.println("true" + " " + (System.currentTimeMillis() - start) + " ms");
+                    } else {
+                        System.out.println("false" + " " + (System.currentTimeMillis() - start) + " ms");
+                    }
+
+                    System.out.println("Search TreeSet");
+                    //Collections.sort(vipNumbers);
+                    if (treeSetVipNumbers.contains(enteredNumber)) {
+                        System.out.println("true" + " " + (System.currentTimeMillis() - start) + " ms");
+                    } else {
+                        System.out.println("false" + " " + (System.currentTimeMillis() - start) + " ms");
+                    }
+
+
+                    if (enteredNumber.equals("STOP")){
+                        System.out.println("Received \"STOP\" command, exit");
+                        break;
+                    }
+                }
+            }
 
 }
